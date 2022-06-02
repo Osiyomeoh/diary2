722eae56c5b2eb1fa37ce29211ab77be1fdd00d0
Osiyomeoh <samuelalenomoh5@gmail.com>
2022-06-02 15:39:07 -0700

    new

--- index.php
+++ index.php
@@ -1,57 +1,136 @@
 <?php
- session_start();
- $error = "";
 
-if(array_key_exists("Logout", $_GET)){
-    unset($_SESSION);
-    setcookie("id", "", time() - 60*60);
-    $_COOKIE["id"] = "";
-} else if(array_key_exists("id",$_SESSION) OR array_key_exists("id",$_COOKIE)){
-    header("Location: loggedInpage.php");
-}
+    session_start();
 
-  if (array_key_exists("submit", $_POST)){
-      $link = mysqli_connect("localhost", "root", "", "diary");
-      if(mysqli_connect_error()){
-          die("Database Connection Error");
-      }
-     
-      if (!$_POST['email']){
-          $error .= "An email address is required<br>";
-      }
-      if (!$_POST['password']){
-        $error .= "A Password is required<br>";
+    $error = "";    
+
+    if (array_key_exists("logout", $_GET)) {
+        
+        unset($_SESSION);
+        setcookie("id", "", time() - 60*60);
+        $_COOKIE["id"] = "";  
+        
+    } else if ((array_key_exists("id", $_SESSION) AND $_SESSION['id']) OR (array_key_exists("id", $_COOKIE) AND $_COOKIE['id'])) {
+        
+        header("Location: loggedInpage.php");
+        
     }
-    if($error != ""){
-        $error = "<p>There were error(s) in your form</p>".$error;
-    } else {
-        $query = "SELECT id FROM `users` WHERE email ='".mysqli_real_escape_string($link, $_POST['email'])."' LIMIT 1";
-        $result = mysqli_query($link, $query);
-        if(mysqli_num_rows($result) > 0){
-            $error = "That email address is taken.";
-        } else {
-            $query = "INSERT INTO `users` (`email`, `password`) VALUES ('".mysqli_real_escape_string($link, $_POST['email'])."',
-            '".mysqli_real_escape_string($link, $_POST['password'])."')";
-            if (!mysqli_query($link, $query)){
-                $error = "<p>Could not sign you up, Please try again later.</p>";
-            } else {
-                $query = "UPDATE `users` SET `password` = '".md5(md5(mysqli_insert_id($link)).$_POST['password'])."'WHERE id = ".mysqli_insert_id($link)."LIMIT 1";
-                mysqli_query($link, $query);
-                $_SESSION['id'] = mysqli_insert_id($link);
-                if($_POST['stayLoggedIn'] == '1'){
-                    setcookie("id",mysqli_insert_id($link), time() + 60+60+24+365);
-                }
-              header("Location: loggedInpage.php");
-            }
 
+    if (array_key_exists("submit", $_POST)) {
+        
+        $link = mysqli_connect("localhost", "root", "", "diary");
+        
+        if (mysqli_connect_error()) {
+            
+            die ("Database Connection Error");
             
         }
+        
+        
+        
+        if (!$_POST['email']) {
+            
+            $error .= "An email address is required<br>";
+            
+        } 
+        
+        if (!$_POST['password']) {
+            
+            $error .= "A password is required<br>";
+            
+        } 
+        
+        if ($error != "") {
+            
+            $error = "<p>There were error(s) in your form:</p>".$error;
+            
+        } else {
+            
+            if ($_POST['signUp'] == '1') {
+            
+                $query = "SELECT id FROM `users` WHERE email = '".mysqli_real_escape_string($link, $_POST['email'])."' LIMIT 1";
+
+                $result = mysqli_query($link, $query);
+
+                if (mysqli_num_rows($result) > 0) {
+
+                    $error = "That email address is taken.";
+
+                } else {
+
+                    $query = "INSERT INTO `users` (`email`, `password`) VALUES ('".mysqli_real_escape_string($link, $_POST['email'])."', '".mysqli_real_escape_string($link, $_POST['password'])."')";
+
+                    if (!mysqli_query($link, $query)) {
+
+                        $error = "<p>Could not sign you up - please try again later.</p>";
+
+                    } else {
+
+                        $query = "UPDATE `users` SET password = '".md5(md5(mysqli_insert_id($link)).$_POST['password'])."' WHERE id = ".mysqli_insert_id($link)." LIMIT 1";
+
+                        mysqli_query($link, $query);
+
+                        $_SESSION['id'] = mysqli_insert_id($link);
+
+                        if ($_POST['stayLoggedIn'] == '1') {
+
+                            setcookie("id", mysqli_insert_id($link), time() + 60*60*24*365);
+
+                        } 
+
+                        header("Location: loggedInpage.php");
+
+                    }
+
+                } 
+                
+            } else {
+                    
+                    $query = "SELECT * FROM `users` WHERE email = '".mysqli_real_escape_string($link, $_POST['email'])."'";
+                
+                    $result = mysqli_query($link, $query);
+                
+                    $row = mysqli_fetch_array($result);
+                
+                    if (isset($row)) {
+                        
+                        $hashedPassword = md5(md5($row['id']).$_POST['password']);
+                        
+                        if ($hashedPassword == $row['password']) {
+                            
+                            $_SESSION['id'] = $row['id'];
+                            
+                            if ($_POST['stayLoggedIn'] == '1') {
+
+                                setcookie("id", $row['id'], time() + 60*60*24*365);
+
+                            } 
+
+                            header("Location: loggedInpage.php");
+                                
+                        } else {
+                            
+                            $error = "That email/password combination could not be found.";
+                            
+                        }
+                        
+                    } else {
+                        
+                        $error = "That email/password combination could not be found.";
+                        
+                    }
+                    
+                }
+            
+        }
+        
+        
     }
-  }
+
+
 ?>
 
-
-<div id="error"></div>
+<div id="error"><?php echo $error; ?></div>
 
 <form method="post">
 
